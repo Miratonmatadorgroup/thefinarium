8b20a83a32d026bf178586f290511fdbffa68c3a
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2025-04-15 18:31:34 +0100

    ci: develop dockerfile for easy and automated deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react(),],
-  
-})
+  plugins: [tailwindcss(), react()],
+  preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: ["thefinarium.com", "www.thefinarium.com"],
+    historyApiFallback: true,
+  },
+});
